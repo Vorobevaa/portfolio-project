c80c6b33fa2c857e7e01140f29d6919187bf71ff
AlexandraVorobeva <vorobeva.alexa@gmail.com>
2020-04-18 08:53:25 +0100

    Add class based views

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-import jobs.views
+from jobs.views import Home
 from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', jobs.views.home, name='home'),
+    path('', Home.as_view(), name='home'),
     path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
