3ef1e3e13e04d37297692d9700fa67e8281a388b
AlexandraVorobeva <vorobeva.alexa@gmail.com>
2020-06-22 13:56:10 +0100

    Change settings, made migrations

---
+++ portfolio/settings/development.py
@@ -0,0 +1,13 @@
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'portfoliodb',
+        'USER': 'root',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
